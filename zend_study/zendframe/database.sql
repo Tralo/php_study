168d65f353a95cd95a9fc7d49986f12903ff6d78
Tralo <13580548169@163.com>
2016-08-07 09:50:15 +0800

    投票系统demo

--- zend_study/zendframe/database.sql
+++ zend_study/zendframe/database.sql
@@ -6,3 +6,21 @@ CREATE TABLE `message`(
 	`content` varchar(2000) NOT NULL,
 	PRIMARY KEY (`message_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE item(
+	id bigint unsigned primary key auto_increment,
+	name varchar(64) not null,
+	description varchar(128) not null,
+	vote_count bigint unsigned) engine = MyISAM DEFAULT CHARSET=utf8;
+
+create table vote_log(
+	id bigint unsigned primary key auto_increment,
+	ip varchar(20) not null,
+	vote_date bigint not null,
+	item_id bigint not null
+) engine = MyISAM DEFAULT CHARSET=utf8;
+
+create table filter(
+	id bigint unsigned primary key auto_increment,
+	ip varchar(20)
+) engine MyISAM;
