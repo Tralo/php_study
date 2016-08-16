05987b1a488e4fe7639fc934b3e57b62ae0661b2
Tralo <13580548169@163.com>
2016-08-16 21:59:58 +0800

    update

---
+++ showImmoc/lib/string.func.php
@@ -0,0 +1,19 @@
+<?php
+
+function f(){
+	$type=1;
+	$length = 4;
+	if($type == 1){
+		$chars = join("",range(0,9));
+	} elseif($type == 2){
+		$chars = join("",array_merge(range("a","z"),range("A","Z")));
+	} elseif ($type == 3) {
+		$chars = join("",array_merge(range("a","z"),range("A","Z"),range("0","9")));
+	}
+	if($length > strlen($chars)){
+		exit("字符串长度不够");
+	}
+	$chars = str_shuffle($chars);//随机打乱顺序
+	substr($chars, 0,$length);
+}
+
