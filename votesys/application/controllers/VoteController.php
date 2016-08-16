6f812dbe2604a65623d19c571271f9cc136e7a2e
Tralo <13580548169@163.com>
2016-08-16 20:05:49 +0800

    update

--- votesys/application/controllers/VoteController.php
+++ votesys/application/controllers/VoteController.php
@@ -7,6 +7,7 @@ require_once 'Zend/Mail/Transport/Smtp.php';
 
 require_once 'BaseController.php';
 require_once APPLICATION_PATH.'/models/Item.php';
+require_once APPLICATION_PATH.'/models/VoteLog.php';
 class VoteController extends BaseController{
     public function indexAction()
     {
@@ -15,6 +16,51 @@ class VoteController extends BaseController{
 
 
     public function voteAction(){
+
+        //获取用户投票的id
+        $item_id = $this->getRequest()->getParam('itemid','no');
+
+        $ip = $this->getRequest()->getServer('REMOTE_ADDR');
+
+        $today = date('Ymd');
+
+        // echo $item_id. '--'.$ip;
+        //先看看vote_log这个表中是否有投过一次了
+        $voteLogModel = new VoteLog();
+        //sql注入不考虑
+        $where = "ip = $ip AND vote_date=$today";
+
+        $res = $voteLogModel->fetchAll($where)->toArray();
+        echo $today.'--';
+        echo '<br>'.$where;
+        echo count($res);
+        if(count($res) > 0){
+            //提示
+        } else {
+
+            $date = array(
+                'ip'=>$ip,
+                'vote_date'=>$today,
+                'item_id'=>$item_id
+            );
+            if($voteLogModel->insert($date) > 0){
+                //更新
+                $itemModel = new Item();
+                //通过逐渐直接获取对应的item
+                $item = $itemModel->find($item_id);
+
+
+                
+            }
+
+
+
+            
+        }
+        
+      
+        exit();
+
         
     }
 
@@ -24,7 +70,7 @@ class VoteController extends BaseController{
         // $this->_hepler->layout()->disableLayout();
         // $this->_hepler->viewRenderer->setNotRender(true);
 
-        $config = array('ssl' => 'tls', 'port' => 2525, 'auth' => 'login', 
+       /* $config = array('ssl' => 'tls', 'port' => 2525, 'auth' => 'login', 
                  'username' => 'shaiazran', 'password' => 'Azran1200');
         $transport = new Zend_Mail_Transport_Smtp('smtp.sendgrid.net', $config);
         $email = trim($this->getRequest()->getPost('email'));
@@ -34,9 +80,9 @@ class VoteController extends BaseController{
         $mail->setDefaultTransport($transport);
 
         // $mail->setFrom('ops@abconcepts.com.hk', 'shaiazran');
-        /*$mail->addHeader('MIME-Version', '1.0');
+        $mail->addHeader('MIME-Version', '1.0');
         $mail->addHeader('Content-Transfer-Encoding', '8bit');
-        $mail->addHeader('X-Mailer:', 'PHP/'.phpversion());*/
+        $mail->addHeader('X-Mailer:', 'PHP/'.phpversion());
 
         $mail->setFrom('app@expressproj.com', 'Express APP');
         $mail->addTo($email,'');
@@ -44,7 +90,7 @@ class VoteController extends BaseController{
         $mail->setBodyText('This is the text of the mail using fdsfdfdfsfdfdfasdfasfdasdfasdZend.');
 
         $mail->send($transport);
-        exit();
+        exit();*/
         
         // exit();
 
